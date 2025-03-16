9a81ef62d0b95bbbf3273c063140cd4467ce3c09
Cauã Henrique <144502865+CauaHm@users.noreply.github.com>
2025-03-16 16:12:05 -0300

    Adicionando animações e organizando arquivors

--- src/scripts/btnMobile.js
+++ src/scripts/btnMobile.js
@@ -5,51 +5,88 @@ $(document).ready(function(){
         $('.navList_Mobile').toggleClass('active');
         $('.btn_Mobile').find('i').toggleClass('fa-x');
     });
-
-    const sections = $('section');
-    const navItems = $(".navList_item");
-
+    
     $(window).on('scroll', function() {
         const header = $('header');
         const scrollPosition = $(window).scrollTop() - header.outerHeight();
-
-        let activeSectionIndex = 0;
 
         if(scrollPosition <= 0) {
             header.css('box-shadow', 'none');
         } else {
             header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
         }
-
-        sections.each(function(i) {
-            const section = $(this);
-            const sectionTop = section.offset().top - 100;
-            const sectionBot = sectionTop + section.outerHeight();
-
-            if (scrollPosition >= sectionTop && scrollPosition < sectionBot) {
-                activeSectionIndex = i;
-                return false;
-            }
-        })
-
-        navItems.removeClass('active')
-        $(navItems[activeSectionIndex]).addClass('active')
     });
 
-    ScrollReveal().reveal('.cta', {
+    window.revelar = ScrollReveal({reset:true});
+
+    revelar.reveal('.cta', {
+        duration: 2000,
         origin: 'left', 
-        duration: 3000,
         distance: '70%' 
     });
-    ScrollReveal().reveal('.banner', {
-        origin: 'right', 
-        duration: 2500,
-        distance: '20%' 
+    revelar.reveal('.banner', {
+        duration: 2000,
+        distance: '20%',
+        delay: 500
     });
-    ScrollReveal().reveal('.projects', {
+    revelar.reveal('.section-title', {
+        duration: 2000,
         origin: 'right', 
-        duration: 2500,
-        distance: '20%' 
+        distance: '20%',   
     });
+
+    revelar.reveal('.container-form', {
+        duration: 2000,
+        origin: 'left', 
+        distance: '20%',  
+        delay: 500
+    });
+
+    revelar.reveal('.categoriesBtn', {
+        duration: 2000,
+        distance: '20%',
+        delay: 250
+    });
+
+    revelar.reveal('.whatWeDo_txt-title', {
+        duration: 2000, 
+        distance: '20%', 
+        origin: 'right',
+    });
+    revelar.reveal('.whatWeDo_list-reveal', {
+        duration: 2000, 
+        distance: '20%', 
+        delay: 500
+    });
+    revelar.reveal('.bid-card-title-reveal', {
+        duration: 2000, 
+        origin: 'right',
+        distance: '20%', 
+    });
+
+    revelar.reveal('.card', {
+        duration: 2000, 
+        distance: '20%', 
+    });
+
+    revelar.reveal('.finderContact-title-reveal', {
+        origin: 'right',
+        duration: 2000, 
+        distance: '20%', 
+    });
+
+    revelar.reveal('.contactSocial form div', {
+        duration: 2000, 
+        distance: '20%', 
+        origin: 'right',
+    });
+
+    revelar.reveal('.finderLogo-reveal', {
+        duration: 2000, 
+        distance: '90px', 
+        delay: 500
+    });
+
+    
 
 });
---
+++ src/scripts/localization.js
@@ -0,0 +1,33 @@
+//Menu Hamburguer
+
+$(document).ready(function(){
+    const sections = $('section');
+    const navItems = $(".navList_item");
+
+    $(window).on('scroll', function() {
+        const header = $('header');
+        const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+        let activeSectionIndex = 0;
+
+        if(scrollPosition <= 0) {
+            header.css('box-shadow', 'none');
+        } else {
+            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1)');
+        }
+
+        sections.each(function(i) {
+            const section = $(this);
+            const sectionTop = section.offset().top - 100;
+            const sectionBot = sectionTop + section.outerHeight();
+
+            if (scrollPosition >= sectionTop && scrollPosition < sectionBot) {
+                activeSectionIndex = i;
+                return false;
+            }
+        })
+
+        navItems.removeClass('active')
+        $(navItems[activeSectionIndex]).addClass('active')
+    });
+});
