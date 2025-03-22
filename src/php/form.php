595a32ebac18a1105e6251d75329bd5170c584f7
Cauã Henrique <144502865+CauaHm@users.noreply.github.com>
2025-03-22 18:31:32 -0300

    fazendo envio de duvida

---
+++ src/php/form.php
@@ -0,0 +1,22 @@
+<?php 
+    $nome = $_POST['nome'] ?? 'VAZIO';
+    $email = $_POST['email'] ?? 'VAZIO';
+    $cel = $_POST['cel'] ?? 'VAZIO';
+    $msg = $_POST['msg'] ?? 'VAZIO';
+
+    $para = "cauamh321@gmail.com";
+    $assunto = "Duvidas - FINDER";
+
+    $mensagemCompleta = "
+    Nome: {$nome}\n
+    Email: {$email}\n
+    Telefone: {$email}\n\n
+    Mensagem de duvida: {$msg}
+    ";
+
+    if(mail($para, $assunto, $mensagemCompleta)) {
+        echo "<script>alert('E-mail enviado com sucesso!')</script>";
+    } else {
+        echo "<script>alert('ERRO, verifique os campos')</script>";
+    }
+?>
